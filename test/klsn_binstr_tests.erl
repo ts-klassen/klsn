a07bc4be6de656034a454bee3e6e454371e464f1
ts-klassen <git@su-shiki.com>
2024-11-17 00:13:59 +0900

    manual fix ai code

--- test/klsn_binstr_tests.erl
+++ test/klsn_binstr_tests.erl
@@ -7,7 +7,7 @@ urlencode_1_test() ->
     ?assertEqual(<<>>, klsn_binstr:urlencode(<<>>)),
     
     %% Test encoding of a string with only safe characters
-    ?assertEqual(<<"HelloWorld123-_~">>, klsn_binstr:urlencode(<<"HelloWorld123-_~">>)),
+    ?assertEqual(<<"HelloWorld123-_~.">>, klsn_binstr:urlencode(<<"HelloWorld123-_~.">>)),
     
     %% Test encoding of a string with unsafe characters
     ?assertEqual(<<"Hello%20World%21">>, klsn_binstr:urlencode(<<"Hello World!">>)),
@@ -16,12 +16,18 @@ urlencode_1_test() ->
     ?assertEqual(<<"foo%2Fbar%3Fbaz%3Dqux">>, klsn_binstr:urlencode(<<"foo/bar?baz=qux">>)),
     
     %% Test encoding of non-ASCII characters
-    ?assertEqual(<<"%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF">>, klsn_binstr:urlencode(<<"こんにちは">>)),
+    ?assertEqual(<<"%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF">>, klsn_binstr:urlencode(<<"こんにちは"/utf8>>)),
     
     %% Test encoding of special characters
-    ?assertEqual(<<"%25%26%2B%2C%2F%3A%3B%3D%3F%40">>, klsn_binstr:urlencode(<<"%&+,/:;=?@">>)),
+    ?assertEqual(<<"%20%09%0D%0A">>, klsn_binstr:urlencode(<<" \t\r\n">>)),
     
-    %% Test encoding of hexadecimal boundary characters
-    ?assertEqual(<<"%00%FF">>, klsn_binstr:urlencode(<<0, 255>>)),
+    %% Test encoding of chars from 32 to 126
+    BinStr = iolist_to_binary(lists:seq(32, 126)),
+    Expect = <<
+        "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C-.%2F0123456789",
+        "%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E"
+        "_%60abcdefghijklmnopqrstuvwxyz%7B%7C%7D~"
+    >>,
+    ?assertEqual(Expect, klsn_binstr:urlencode(BinStr)),
     
     ok.
