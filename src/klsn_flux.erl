63dc4b5b05664dcbc20927fad67e17d6d778a97c
ts-klassen <git@su-shiki.com>
2025-03-20 19:12:21 +0900

    added query for klsn_flux

--- src/klsn_flux.erl
+++ src/klsn_flux.erl
@@ -3,10 +3,14 @@
 -export([
         write/3
       , write/4
-%     , flux_query/3
-%     , flux_query/4
+      , flux_query/2
+      , flux_query/3
+      , q/3
+      , q/4
+      , value/1
       , timestamp/0
       , points_to_line_protocol/1
+      , csv/1
     ]).
 -export_type([
         info/0
@@ -36,6 +40,27 @@
     }.
 -type organization() :: key().
 -type bucket() :: key().
+-type unit() :: d | h | m | s.
+-type value() ::
+    {object, maps:map(Identifier::value(), Value::value())}
+  | Object::maps:map(Identifier::value(), Value::value())
+  | {array, Array::[Element::value()]}
+  | Array::[Element::value()]
+  | {unary, Operator::klsn:binstr(), Value::value()}
+  | {call, value()}
+  | {bool, Bool::boolean()} | Bool::boolean()
+  | {identifier, Name::key()} | Identifier::atom()
+  | {int, Int::integer()} | Int::integer()
+  | {uint, UInt::non_neg_integer()}
+  | {uint, Float::float()} | Float::float()
+  | {string, String::klsn:binstr()} | String::klsn:binstr()
+  | {duration, [{Magnitude::integer(), unit()}]}
+  | {date_time, DateTime::klsn:binstr()}
+  | {timestamp, NanoSecond::integer()}
+  | {unixtime, Second::integer()}
+  | {regex, DateTime::klsn:binstr()}
+  | {raw, #{}}
+  .
 
 -spec post(#{
         q := #{}
@@ -59,8 +84,8 @@ post(Request, #{uri_map:=UriMap, headers:=Headers}) ->
     case Res of
         {ok, {{_, Stat, _}, _, Data}} when 200=<Stat,Stat=<299 ->
             Data;
-        {ok, {{_, Stat, _}, _, _}} ->
-            error({klsn_flux_status_error, Stat});
+        {ok, {{_, Stat, _}, _, Data}} ->
+            error({klsn_flux_status_error, Stat, Data});
         {error, Error} ->
             error({klsn_flux_httpc_error, Error})
     end.
@@ -102,13 +127,110 @@ write_(Org, Bucket, Body, Info, Retry) ->
         <<>> ->
             ok
     catch
-        error:{klsn_flux_status_error, 500} ->
+        error:{klsn_flux_status_error, 500, _} ->
             sleep(Retry),
             write_(Org, Bucket, Body, Info, Retry+1);
         Class:Error:Stack ->
             spawn(fun()-> erlang:raise(Class,Error,Stack) end),
             sleep(Retry),
             write_(Org, Bucket, Body, Info, Retry+5)
+    end.
+
+
+-spec q(
+        organization()
+      , Query::klsn:binstr()
+      , Args::value()
+    ) -> maps:map(klsn:binstr(), klsn:binstr()).
+q(Org, Query, Args) ->
+    q(Org, Query, Args, info()).
+
+
+-spec q(
+        organization()
+      , Query::klsn:binstr()
+      , Args::value()
+      , info()
+    ) -> maps:map(klsn:binstr(), klsn:binstr()).
+q(Org, Query, Args, Info) ->
+    CSV = flux_query(Org, #{
+        'query' => Query
+      , extern => #{
+            type => 'File'
+          , package => null
+          , imports => null
+          , body => [#{
+                type => 'OptionStatement'
+              , assignment => #{
+                    type => 'VariableAssignment'
+                  , id => value(args)
+                  , init => value(Args)
+                }
+            }]
+        }
+    }, Info),
+    [Header|Body] = csv(CSV),
+    HeaderLength = length(Header),
+    lists:filtermap(fun(Row)->
+        case length(Row) of
+            Len when Len =:= HeaderLength ->
+                {true, maps:from_list(lists:zip(tl(Header), tl(Row)))};
+            _ ->
+                false
+        end
+    end, Body).
+
+
+-spec flux_query(
+        organization()
+      , klsn:binstr() | #{}
+    ) -> ok.
+flux_query(Org, Query) ->
+    flux_query(Org, Query, info()).
+
+-spec flux_query(
+        organization()
+      , klsn:binstr() | #{}
+      , info()
+    ) -> ok.
+flux_query(_Org, [], _Info) ->
+    ok;
+flux_query(Org, Query, Info) ->
+    flux_query_(Org, Query, Info, 1).
+
+flux_query_(_Org, _Query, _Info, ReTry) when ReTry >= 10 ->
+    error(too_many_retry);
+flux_query_(Org, Query, Info, Retry) ->
+    try
+        post(#{
+            q => #{
+                <<"org">> => klsn_binstr:from_any(Org)
+            }
+          , path => <<"/api/v2/query">>
+          , ctype => case Query of
+                #{} ->
+                    "application/json";
+                _ ->
+                    "application/vnd.flux"
+            end
+          , body => case Query of
+                #{} ->
+                    jsone:encode(Query);
+                _ ->
+                    Query
+            end
+        }, Info)
+    of
+        Res ->
+            Res
+    catch
+        error:{klsn_flux_status_error, 500, _} ->
+            sleep(Retry),
+            flux_query_(Org, Query, Info, Retry+1);
+        Class:Error:Stack ->
+            spawn(fun()-> erlang:raise(Class,Error,Stack) end),
+            sleep(Retry),
+            flux_query_(Org, Query, Info, Retry+5)
     end.
 
 
@@ -196,4 +318,144 @@ info() ->
 sleep(Stage) ->
     timer:sleep(round(1000 * rand:uniform() + 100 * math:exp(Stage))).
 
+
+value({object, Properties}) ->
+    #{
+        type => 'ObjectExpression'
+      , properties => lists:map(fun({Key, Value})->
+            #{
+                type => 'Property'
+              , key => value(Key)
+              , value => value(Value)
+            }
+        end, maps:to_list(Properties))
+    };
+value({array, Elements}) ->
+    #{
+        type => 'ArrayExpression'
+      , elements => lists:map(fun(Value)->
+            value(Value)
+        end, Elements)
+    };
+value({unary, Operator, Value}) ->
+    #{
+        type => 'UnaryExpression'
+      , operator => Operator
+      , argument => value(Value)
+    };
+value({call, Value}) ->
+    #{
+        type => 'CallExpression'
+      , callee => value(Value)
+    };
+value({bool, Bool}) ->
+    #{
+        type => 'BooleanLiteral'
+      , value => Bool
+    };
+value({identifier, Identifier}) ->
+    #{
+        type => 'Identifier'
+      , name => Identifier
+    };
+value({int, Int}) ->
+    #{
+        type => 'IntegerLiteral'
+      , value => Int
+    };
+value({uint, UInt}) ->
+    #{
+        type => 'UnsignedIntegerLiteral'
+      , value => UInt
+    };
+value({float, Float}) ->
+    #{
+        type => 'FloatLiteral'
+      , value => Float
+    };
+value({string, String}) ->
+    #{
+        type => 'StringLiteral'
+      , value => String
+    };
+value({duration, Args}) ->
+    #{
+        type => 'DurationLiteral'
+      , values => lists:map(fun({Magnitude, Unit})->
+            #{
+                magnitude => Magnitude
+              , unit => Unit
+            }
+        end, Args)
+    };
+value({date_time, DateTime}) ->
+    #{
+        type => 'DateTimeLiteral'
+      , values => DateTime
+    };
+value({regex, Regex}) ->
+    #{
+        type => 'RegexpLiteral'
+      , values => Regex
+    };
+value({raw, Raw}) ->
+    Raw;
+
+value(true) ->
+    value({bool, true});
+value(false) ->
+    value({bool, false});
+value(Identifier) when is_atom(Identifier) ->
+    value({identifier, Identifier});
+value(Int) when is_integer(Int) ->
+    value({int, Int});
+value(Float) when is_float(Float) ->
+    value({float, Float});
+value(String) when is_binary(String) ->
+    value({string, String});
+value(Object) when is_map(Object) ->
+    value({object, Object});
+value(Elements) when is_list(Elements) ->
+    value({array, Elements});
+value({timestamp, Timestamp}) ->
+    value({date_time, iolist_to_binary(calendar:system_time_to_rfc3339(Timestamp, [{unit, nanosecond}]))});
+value({unixtime, Unixtime}) ->
+    value({date_time, iolist_to_binary(calendar:system_time_to_rfc3339(Unixtime, [{unit, second}]))});
+
+value(Arg) ->
+    erlang:error(badarg, [Arg]).
+
+
+-spec csv(klsn:binstr()) -> [[klsn:binstr()]].
+csv(CSV) ->
+    csv(CSV, normal, [[<<>>]]).
+
+-spec csv(
+        klsn:binstr()
+      , normal | quote
+      , [[klsn:binstr()]]
+    ) -> [[klsn:binstr()]].
+csv(<<>>, _, [Row|Res]) ->
+    lists:reverse([lists:reverse(Row)|Res]);
+csv(<<"\\", C:1/binary, Tail/binary>>, State, [[Bin|Row]|Res]) ->
+    E = case C of
+        $\\ -> $\\;
+        $n -> $\n;
+        $r -> $\r;
+        $t -> $\t;
+        _ -> C
+    end,
+    csv(Tail, State, [[<<Bin/binary, E:1/binary>>|Row]|Res]);
+csv(<<"\r\n", Tail/binary>>, normal, [Row|Res]) ->
+    csv(Tail, normal, [[<<>>], lists:reverse(Row) | Res]);
+csv(<<"\n", Tail/binary>>, normal, [Row|Res]) ->
+    csv(Tail, normal, [[<<>>], lists:reverse(Row) | Res]);
+csv(<<",", Tail/binary>>, normal, [Row|Res]) ->
+    csv(Tail, normal, [[<<>>|Row]|Res]);
+csv(<<"\"", Tail/binary>>, normal, Res) ->
+    csv(Tail, quote, Res);
+csv(<<"\"", Tail/binary>>, quote, Res) ->
+    csv(Tail, normal, Res);
+csv(<<C:1/binary, Tail/binary>>, State, [[Bin|Row]|Res]) ->
+    csv(Tail, State, [[<<Bin/binary, C:1/binary>>|Row]|Res]).
 
