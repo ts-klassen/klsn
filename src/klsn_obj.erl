3b9fe0c554e29d4d190f8e383dbf9077f9e8c882
ts-klassen <git@su-shiki.com>
2025-05-11 15:23:49 +0900

    fix klsn_obj:get/2 and lookup/2

--- src/klsn_obj.erl
+++ src/klsn_obj.erl
@@ -1,11 +1,17 @@
 -module(klsn_obj).
 
 -export([
-        
+        get/2
+      , lookup/2
     ]).
 
 -export_type([
-        
+        value/0
+      , key/0
+      , nth/0
+      , obj/0
+      , cmd/0
+      , path/0
     ]).
 
 -type value() :: term().
@@ -24,10 +30,18 @@
              | nth() | key()
              .
 
--type path() :: [cmd()]
+-type path() :: [cmd()].
 
 
 -spec lookup(path(), obj()) -> klsn:maybe(value()).
+lookup(Path, Obj) ->
+    try get(Path, Obj) of
+        Value ->
+            {value, Value}
+    catch
+        error:not_found ->
+            none
+    end.
 
 -spec get(path(), obj()) -> value().
 get([], Value) ->
@@ -37,49 +51,49 @@ get([H|T], Map) when is_map(Map) ->
         {raw, Key0} -> Key0;
         {map, Key0} -> Key0;
         {m, Key0} -> Key0;
-        {list, _} -> erlang:error(enoent, [[H|T], Map]);
-        {l, _} -> erlang:error(enoent, [[H|T], Map]);
-        {tuple, Key0} -> erlang:error(enoent, [[H|T], Map]);
-        {t, _} -> erlang:error(enoent, [[H|T], Map]);
+        {list, _} -> erlang:error(not_found, [[H|T], Map]);
+        {l, _} -> erlang:error(not_found, [[H|T], Map]);
+        {tuple, Key0} -> erlang:error(not_found, [[H|T], Map]);
+        {t, _} -> erlang:error(not_found, [[H|T], Map]);
         Key0 -> Key0
     end,
     case maps:find(Key, Map) of
         {ok, Value} ->
             get(T, Value);
         error ->
-            erlang:error(enoent, [[H|T], Map])
+            erlang:error(not_found, [[H|T], Map])
     end;
 get([H|T], List) when is_list(List) ->
     Nth = case H of
-        {raw, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        {map, _} -> erlang:error(enoent, [[H|T], Map]);
-        {m, _} -> erlang:error(enoent, [[H|T], Map]);
-        {list, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        {l, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        {tuple, _} -> erlang:error(enoent, [[H|T], Map]);
-        {t, _} -> erlang:error(enoent, [[H|T], Map]);
-        Key0 when is_integer(Key0); (Key0 > 0) -> Key0;
-        _ -> erlang:error(enoent, [[H|T], Map])
+        {raw, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        {map, _} -> erlang:error(not_found, [[H|T], List]);
+        {m, _} -> erlang:error(not_found, [[H|T], List]);
+        {list, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        {l, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        {tuple, _} -> erlang:error(not_found, [[H|T], List]);
+        {t, _} -> erlang:error(not_found, [[H|T], List]);
+        Key0 when is_integer(Key0), (Key0 > 0) -> Key0;
+        _ -> erlang:error(not_found, [[H|T], List])
     end,
     try lists:nth(Nth, List) catch
         error:function_clause ->
-            erlang:error(enoent, [[H|T], Map])
+            erlang:error(not_found, [[H|T], List])
     end;
 get([H|T], Tuple) when is_tuple(Tuple) ->
     Nth = case H of
-        {raw, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        {map, _} -> erlang:error(enoent, [[H|T], Map]);
-        {m, _} -> erlang:error(enoent, [[H|T], Map]);
-        {list, _} -> erlang:error(enoent, [[H|T], Map]);
-        {l, _} -> erlang:error(enoent, [[H|T], Map]);
-        {tuple, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        {t, Key0} when is_integer(Key0); (Key0 > 0) -> Key0;
-        Key0 when is_integer(Key0); (Key0 > 0) -> Key0;
-        _ -> erlang:error(enoent, [[H|T], Map])
+        {raw, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        {map, _} -> erlang:error(not_found, [[H|T], Tuple]);
+        {m, _} -> erlang:error(not_found, [[H|T], Tuple]);
+        {list, _} -> erlang:error(not_found, [[H|T], Tuple]);
+        {l, _} -> erlang:error(not_found, [[H|T], Tuple]);
+        {tuple, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        {t, Key0} when is_integer(Key0), (Key0 > 0) -> Key0;
+        Key0 when is_integer(Key0), (Key0 > 0) -> Key0;
+        _ -> erlang:error(not_found, [[H|T], Tuple])
     end,
-    try element(Nth, List) catch
+    try element(Nth, Tuple) catch
         error:badarg ->
-            erlang:error(enoent, [[H|T], Map])
+            erlang:error(not_found, [[H|T], Tuple])
     end.
 
 
