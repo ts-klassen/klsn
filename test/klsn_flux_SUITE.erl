861f115c9aa4e28902274c18f5da3015c597bdba
ts-klassen <git@su-shiki.com>
2025-05-31 00:15:11 +0900

    test(klsn_flux): verify data persistence after write in klsn_flux_SUITE

--- test/klsn_flux_SUITE.erl
+++ test/klsn_flux_SUITE.erl
@@ -24,5 +24,25 @@ main(_Config) ->
         }
     },
     ok = klsn_flux:write(Org, Bucket, Point),
+    %% Verify data was persisted by querying InfluxDB
+    Flux = iolist_to_binary(
+               io_lib:format(
+                 "from(bucket: \"~s\") |> range(start: -1m)"
+                 ++
+                 " |> filter(fn: (r) => r._measurement == \"~s\" and r._field == \"test\")",
+                 [atom_to_list(Bucket), binary_to_list(Measurement)])),
+    CSV = klsn_flux:flux_query(Org, Flux),
+    [Header | Rows] = klsn_flux:csv(CSV),
+    Cols = lists:zip(Header, lists:seq(1, length(Header))),
+    {_, MeasIdx} = lists:keyfind(<<"_measurement">>, 1, Cols),
+    {_, ValueIdx} = lists:keyfind(<<"_value">>, 1, Cols),
+    Row = hd(Rows),
+    MeasRaw = lists:nth(MeasIdx, Row),
+    MeasRaw =:= Measurement orelse exit({measurement_mismatch, MeasRaw, Measurement}),
+    ValueRaw = lists:nth(ValueIdx, Row),
+    case ValueRaw of
+        <<"1">>   -> ok;
+        <<"1.0">> -> ok
+    end,
     ok.
 
