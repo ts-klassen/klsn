5ec3772334bf6cd8226f83564a2c4bc1ae0c7b93
ts-klassen <104614062+ts-klassen@users.noreply.github.com>
2025-05-30 23:22:28 +0900

    Tests (#20)
    * add_test_for_binstr
    
    * add_tests_for_code
    
    * added_db_tests

--- test/klsn_code_tests.erl
+++ test/klsn_code_tests.erl
@@ -1,14 +1,35 @@
 -module(klsn_code_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-export_type([
+        sample_type/0
+      , sample_type/1
+      , sample_type/2
+    ]).
+
+-type sample_type() :: sample_type_payload.
+-type sample_type(A) :: A.
+-type sample_type(A, B) :: {A,B}.
+
+-spec sample_function() -> sample_function_return.
+sample_function() ->
+    sample_function_return.
+
+-spec sample_function(sample_function_arg1) -> sample_function_return1;
+                     (sample_function_arg2) -> sample_function_return2.
+sample_function(sample_function_arg1) ->
+    sample_function_return1;
+sample_function(sample_function_arg2) ->
+    sample_function_return2.
+
 %% Tests for type/1
 type_1_test() ->
     ?assertMatch(
         {sample_type,{atom,{_,_},sample_type_payload},[]}
-      , klsn_code:type({klsn_code, sample_type, 0})),
+      , klsn_code:type({klsn_code_tests, sample_type, 0})),
     ?assertMatch(
         {sample_type,{var,{_,_},'A'},[{var,{_,_},'A'}]}
-      , klsn_code:type({klsn_code, sample_type, 1})),
+      , klsn_code:type({klsn_code_tests, sample_type, 1})),
     ?assertMatch(
         {sample_type,
             {type,
@@ -16,7 +37,9 @@ type_1_test() ->
                 tuple,
                 [{var,{_,_},'A'},{var,{_,_},'B'}]},
             [{var,{_,_},'A'},{var,{_,_},'B'}]}
-      , klsn_code:type({klsn_code, sample_type, 2})),
+      , klsn_code:type({klsn_code_tests, sample_type, 2})),
+    ?assertError(undefined_type, klsn_code:type({klsn_code_tests, sample_type, 3})),
+    ?assertError(no_abstract_code, klsn_code:type({klsn_code_tests_1, sample_type, 3})),
     ok.
 
 %% Tests for spec/1
@@ -27,7 +50,7 @@ spec_1_test() ->
              'fun',
              [{type,{_,_},product,[]},
               {atom,{_,_},sample_function_return}]}]
-      , klsn_code:spec({klsn_code, sample_function, 0})),
+      , klsn_code:spec({klsn_code_tests, sample_function, 0})),
     ?assertMatch(
         [{type,
              {_,_},
@@ -45,7 +68,8 @@ spec_1_test() ->
                   product,
                   [{atom,{_,_},sample_function_arg2}]},
               {atom,{_,_},sample_function_return2}]}]
-      , klsn_code:spec({klsn_code, sample_function, 1})),
+      , klsn_code:spec({klsn_code_tests, sample_function, 1})),
+    ?assertError(undefined_spec, klsn_code:spec({klsn_code_tests, sample_function, 3})),
     ok.
 
 %% Tests for function/1
@@ -55,7 +79,7 @@ function_1_test() ->
              {_,_},
              [],[],
              [{atom,{_,_},sample_function_return}]}]
-      , klsn_code:function({klsn_code, sample_function, 0})),
+      , klsn_code:function({klsn_code_tests, sample_function, 0})),
     ?assertMatch(
         [{clause,
              {_,_},
@@ -67,6 +91,7 @@ function_1_test() ->
              [{atom,{_,_},sample_function_arg2}],
              [],
              [{atom,{_,_},sample_function_return2}]}]
-      , klsn_code:function({klsn_code, sample_function, 1})),
+      , klsn_code:function({klsn_code_tests, sample_function, 1})),
+    ?assertError(undefined_function, klsn_code:function({klsn_code_tests, sample_function, 3})),
     ok.
 
--- test/klsn_db_SUITE.erl
+++ test/klsn_db_SUITE.erl
@@ -24,6 +24,12 @@ main(_Config) ->
             ok
     end,
     {Key, _} = klsn_db:create_doc(DB, #{<<"test_val">> => <<"test123">>}),
+    ok = try
+        klsn_db:create_doc(non_existing, #{})
+    catch
+        error:not_found ->
+            ok
+    end,
     #{<<"test_val">> := <<"test123">>} = klsn_db:get(DB, Key),
     ok = try klsn_db:get(DB, <<"non-existing-key">>) catch
         error:not_found ->
@@ -52,5 +58,39 @@ main(_Config) ->
             map => <<"function (doc) {emit(doc._id, 2);}">>
         }}, language => javascript}
     end),
+    {value, #{}} = klsn_db:lookup(DB, {raw, <<"_design/index">>}),
+    ok = try
+        klsn_db:update(DB, <<"non_existing">>, fun(_) ->
+            error(unreachable)
+        end)
+    catch
+        error:not_found ->
+            ok
+    end,
+    none = klsn_db:lookup(DB, {raw, <<"_design/none">>}),
+    none = klsn_db:lookup(DB, <<>>),
+    #{} = klsn_db:upsert(DB, <<>>, fun(none)-> #{} end),
+    ConflictRef = make_ref(),
+    Self = self(),
+    ConflictPid = spawn_link(fun() ->
+        receive ConflictRef ->
+            ok
+        end,
+        klsn_db:upsert(DB, <<"conflict-once">>, fun(none) ->
+            #{}
+        end),
+        Self ! ConflictRef
+    end),
+    klsn_db:upsert(DB, <<"conflict-once">>, fun
+        (none) ->
+            ConflictPid ! ConflictRef,
+            receive ConflictRef ->
+                ok
+            end,
+            #{};
+        ({value, Doc}) ->
+            Doc
+    end),
+    false = erlang:is_process_alive(ConflictPid),
     ok.
 
